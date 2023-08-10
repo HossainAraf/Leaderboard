5818e01fbdb52e62b190f6c530f51e4a3cfb7ffd
Hossain <arafat.kd99@gmail.com>
2023-08-10 10:18:29 +0100

    Update html & basic style

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
